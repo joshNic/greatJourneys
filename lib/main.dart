4639d01c86233907357d63771069f1b53634c606
joshNic <256jomu@gmail.com>
2020-02-29 23:43:06 +0300

    add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:placesapp/providers/great_palces.dart';
+import 'package:placesapp/screens/places_list_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,22 +9,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My Places',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.indigo,
-        accentColor: Colors.amber
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+        title: 'My Places',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.amber
+        ),
+        home: PlacesListScreen(),
       ),
-      home: ,
     );
   }
 }
