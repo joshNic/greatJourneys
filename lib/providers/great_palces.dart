fc21a7260d0c68c508deb8b2c9ce6304a61f6457
joshNic <256jomu@gmail.com>
2020-03-02 10:48:26 +0300

    adds fetch data from database

--- lib/providers/great_palces.dart
+++ lib/providers/great_palces.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
-import 'package:placesapp/helpers/db_helper.dart';
 
 import '../models/place.dart';
+import '../helpers/db_helper.dart';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
@@ -29,5 +29,20 @@ class GreatPlaces with ChangeNotifier {
       'title': newPlace.title,
       'image': newPlace.image.path,
     });
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final dataList = await DBHelper.getData('user_places');
+    _items = dataList
+        .map(
+          (item) => Place(
+        id: item['id'],
+        title: item['title'],
+        image: File(item['image']),
+        location: null,
+      ),
+    )
+        .toList();
+    notifyListeners();
   }
 }
--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:placesapp/providers/great_palces.dart';
 import 'package:provider/provider.dart';
 
 import './add_place_screen.dart';
+import '../providers/great_palces.dart';
 
 class PlacesListScreen extends StatelessWidget {
   @override
@@ -19,24 +19,33 @@ class PlacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Consumer<GreatPlaces>(
-        child: Center(
-          child: const Text('Got no places yet, start adding some!'),
-        ),
-        builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
-            ? ch
-            : ListView.builder(
-          itemCount: greatPlaces.items.length,
-          itemBuilder: (ctx, i) => ListTile(
-            leading: CircleAvatar(
-              backgroundImage: FileImage(
-                greatPlaces.items[i].image,
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false)
+            .fetchAndSetPlaces(),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+            ConnectionState.waiting
+            ? Center(
+          child: CircularProgressIndicator(),
+        )
+            : Consumer<GreatPlaces>(
+          child: Center(
+            child: const Text('Got no places yet, start adding some!'),
+          ),
+          builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+              ? ch
+              : ListView.builder(
+            itemCount: greatPlaces.items.length,
+            itemBuilder: (ctx, i) => ListTile(
+              leading: CircleAvatar(
+                backgroundImage: FileImage(
+                  greatPlaces.items[i].image,
+                ),
               ),
+              title: Text(greatPlaces.items[i].title),
+              onTap: () {
+                // Go to detail page ...
+              },
             ),
-            title: Text(greatPlaces.items[i].title),
-            onTap: () {
-              // Go to detail page ...
-            },
           ),
         ),
       ),
