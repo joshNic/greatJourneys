9613955f33fcf4f3a240548c3b9b98c3a51357b3
joshNic <256jomu@gmail.com>
2020-03-02 12:26:44 +0300

    add google maps

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 
 class PlaceLocation {
@@ -6,8 +7,11 @@ class PlaceLocation {
   final double longitude;
   final String address;
 
-  PlaceLocation(
-      {@required this.latitude, @required this.longitude, this.address});
+  const PlaceLocation({
+    @required this.latitude,
+    @required this.longitude,
+    this.address,
+  });
 }
 
 class Place {
@@ -16,9 +20,10 @@ class Place {
   final PlaceLocation location;
   final File image;
 
-  Place(
-      {@required this.id,
-      @required this.title,
-      @required this.location,
-      @required this.image});
+  Place({
+    @required this.id,
+    @required this.title,
+    @required this.location,
+    @required this.image,
+  });
 }
