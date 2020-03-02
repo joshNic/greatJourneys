dad2cf28cdfc422057b2c9dc604a652d4b3d8927
joshNic <256jomu@gmail.com>
2020-03-02 09:34:33 +0300

    adds data management via provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:placesapp/providers/great_palces.dart';
-import 'package:placesapp/screens/add_place_screen.dart';
-import 'package:placesapp/screens/places_list_screen.dart';
 import 'package:provider/provider.dart';
+
+import './providers/great_palces.dart';
+import './screens/places_list_screen.dart';
+import './screens/add_place_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider.value(
       value: GreatPlaces(),
       child: MaterialApp(
-        title: 'My Places',
-        theme: ThemeData(
-          primarySwatch: Colors.indigo,
-          accentColor: Colors.amber
-        ),
-        home: PlacesListScreen(),
+          title: 'Great Places',
+          theme: ThemeData(
+            primarySwatch: Colors.indigo,
+            accentColor: Colors.amber,
+          ),
+          home: PlacesListScreen(),
           routes: {
             AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
           }),
-      ),
     );
   }
 }
--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,17 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:placesapp/providers/great_palces.dart';
+import 'package:provider/provider.dart';
+
+import './add_place_screen.dart';
 
 class PlacesListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My Places'),
+        title: Text('Your Places'),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.add), onPressed: null)
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+            },
+          ),
         ],
       ),
-      body: Center(
-        child: CircularProgressIndicator(),
+      body: Consumer<GreatPlaces>(
+        child: Center(
+          child: const Text('Got no places yet, start adding some!'),
+        ),
+        builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+            ? ch
+            : ListView.builder(
+          itemCount: greatPlaces.items.length,
+          itemBuilder: (ctx, i) => ListTile(
+            leading: CircleAvatar(
+              backgroundImage: FileImage(
+                greatPlaces.items[i].image,
+              ),
+            ),
+            title: Text(greatPlaces.items[i].title),
+            onTap: () {
+              // Go to detail page ...
+            },
+          ),
+        ),
       ),
     );
   }
